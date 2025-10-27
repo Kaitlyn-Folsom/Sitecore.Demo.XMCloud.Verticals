08d54830ca2cfb681c17ece8bac33938ca99c01c
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2025-10-27 19:38:03 -0400

    upgrade jss to 22.6

--- src/sxastarter/src/pages/[[...path]].tsx
+++ src/sxastarter/src/pages/[[...path]].tsx
@@ -7,6 +7,7 @@ import { handleEditorFastRefresh } from '@sitecore-jss/sitecore-jss-nextjs/utils
 import { SitecorePageProps } from 'lib/page-props';
 import { sitecorePagePropsFactory } from 'lib/page-props-factory';
 import { componentBuilder } from 'temp/componentBuilder';
+import config from 'temp/config';
 
 const SitecorePage = ({
   notFound,
@@ -31,6 +32,12 @@ const SitecorePage = ({
       <SitecoreContext
         componentFactory={componentBuilder.getComponentFactory({ isEditing })}
         layoutData={layoutData}
+        api={{
+          edge: {
+            contextId: config.sitecoreEdgeContextId,
+            edgeUrl: config.sitecoreEdgeUrl,
+          },
+        }}
       >
         <Layout layoutData={layoutData} headLinks={headLinks} />
       </SitecoreContext>
